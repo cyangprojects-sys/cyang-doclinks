2934128302e49a37f4bd9ca0ff9908dbf7543986
Chang Yang <cyangprojects@gmail.com>
2026-01-21 14:40:39 -0600

    Fix postgres sql typing for Vercel build

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,11 @@
-import postgres from "postgres";
+import postgres, { type Sql } from "postgres";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("Missing DATABASE_URL");
 }
 
-export const sql = postgres(process.env.DATABASE_URL, {
+// 👇 Explicitly type sql as Sql
+export const sql: Sql = postgres(process.env.DATABASE_URL, {
   ssl: "require",
   max: 5,
 });
