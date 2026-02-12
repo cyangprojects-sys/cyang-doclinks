fe97a71d6045a9bf4d20cf71d06b81f2c535dce2
Chang Yang <cyangprojects@gmail.com>
2026-02-12 02:28:09 -0600

    Use getServerSession in admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,12 +1,13 @@
-import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/auth";
 
 export default async function AdminLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    const session = await auth();
+    const session = await getServerSession(authOptions);
     const email = session?.user?.email;
 
     if (!email) redirect("/");
